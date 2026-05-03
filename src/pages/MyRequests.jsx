e02aed261ce90d7447046b1a52b2cd02f7e62f44
DavidAustine <davoace345@gmail.com>
2026-05-03 11:59:58 +0100

    changes made

--- src/pages/MyRequests.jsx
+++ src/pages/MyRequests.jsx
@@ -1,7 +1,7 @@
 // src/pages/MyRequests.jsx
 // Page wrapper for the My Requests component
 
-import MyRequestsComponent from "../components/my-requests/MyRequests";
+import MyRequestsComponent from "../components/my-request/MyRequests";
 
 export default function MyRequests() {
   return <MyRequestsComponent />;
